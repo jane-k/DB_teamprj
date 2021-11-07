4b897a0c5bfbebc20e53b2ccea7cf44460062f1c
suhwan cheon <soo7652@naver.com>
2021-11-07 09:41:37 +0900

    init commit

---
+++ src/main/java/db/teamprj/TeamprjApplication.java
@@ -0,0 +1,13 @@
+package db.teamprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamprjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/db/teamprj/TeamprjApplicationTests.java
@@ -0,0 +1,13 @@
+package db.teamprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
